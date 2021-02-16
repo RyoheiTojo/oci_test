99d4f5bdbc874a057f3deb6964f61f900cb1a39f
Ryohei <ryohei.mmeve@gmail.com>
2021-02-16 18:36:04 +0900

    version

--- Test_instances.tf
+++ Test_instances.tf
@@ -5,7 +5,7 @@ module "TestInstance" {
   tenancy_ocid = var.tenancy_ocid
   region = var.region
 
-  instance_shape = "VM.Standard1.1"
+  instance_shape = "VM.Standard2.1"
   subnet_ocid = "ocid1.subnet.oc1.iad.aaaaaaaag2lbmdcl3zlqkfz2fhszxdzbcwgb7m3lrmmzwgqkcy32c3z5ik3a"
   instance_image_ocid = "ocid1.image.oc1.iad.aaaaaaaaissw3yfxkvrijvfkvvfhabosr7tsu322wjm23ds6h4lbour7l5ga"
   ssh_public_key      = var.ssh_public_key
